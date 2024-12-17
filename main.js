621dcc02ed95f642bc496736cac210c1c019ca5e
Atharva Kadam <72775534+AtharvaK123@users.noreply.github.com>
2024-12-17 20:34:41 +0000

    main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let button = document.getElementById("button");
+            button.addEventListener('click', () => {
+                window.location.href = '/my-blogs.html';
+                console.log(1);
+            });
+
+
